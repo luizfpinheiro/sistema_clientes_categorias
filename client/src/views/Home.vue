0ce613f35fe3804004d7b08a0cb47b6de333e0a2
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2020-12-22 01:14:43 -0300

    [UPDATE] Layout inicial + configs vue-router

---
+++ client/src/views/Home.vue
@@ -0,0 +1,100 @@
+<template>
+  <div>
+    <v-container class="grey lighten-5 mb-6">
+      <v-row align="start" no-gutters>
+        <v-col cols="3">
+          <v-card class="mx-auto" width="256">
+            <v-navigation-drawer class="deep-purple accent-4" dark permanent>
+              <v-list>
+                <v-list-item v-for="item in items" :key="item.title" link>
+                  <v-list-item-icon>
+                    <v-icon>{{ item.icon }}</v-icon>
+                  </v-list-item-icon>
+
+                  <v-list-item-content>
+                    <v-list-item-title>{{ item.title }}</v-list-item-title>
+                  </v-list-item-content>
+                </v-list-item>
+              </v-list>
+            </v-navigation-drawer>
+          </v-card>
+        </v-col>
+        <v-col cols="9">
+          <v-data-table
+            :headers="headers"
+            :items="desserts"
+            item-key="name"
+            class="elevation-1"
+            :search="search"
+            hide-default-footer
+          >
+            <template v-slot:top>
+              <v-text-field
+                v-model="search"
+                label="Pesquisa"
+                class="mx-4"
+              ></v-text-field>
+            </template>
+          </v-data-table>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: "Home",
+  data() {
+    return {
+      items: [
+        { title: "Clientes", icon: "mdi-account-box" },
+        { title: "Categorias", icon: "mdi-view-dashboard" },
+      ],
+      search: "",
+      calories: "",
+      headers: [
+        {
+          text: "Dessert (100g serving)",
+          align: "start",
+          sortable: false,
+          value: "name",
+        },
+        {
+          text: "Calories",
+          value: "calories",
+          filter: (value) => {
+            if (!this.calories) return true;
+
+            return value < parseInt(this.calories);
+          },
+        },
+        { text: "Fat (g)", value: "fat" },
+        { text: "Carbs (g)", value: "carbs" },
+        { text: "Protein (g)", value: "protein" },
+        { text: "Iron (%)", value: "iron" },
+      ],
+      desserts: [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: "1%",
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: "1%",
+        },
+      ],
+    };
+  },
+};
+</script>
