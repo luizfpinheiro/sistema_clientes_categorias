8714d094e9eaa975ea1a99b21d5ec1dd8f4fbf0d
Luiz F. Pinheiro <luizfelipenet@hotmail.com>
2020-12-23 03:27:21 -0300

    Configs django

--- api/settings.py
+++ api/settings.py
@@ -130,7 +130,6 @@ ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
-        'rest_framework.renderers.BrowsableAPIRenderer',
         'rest_framework.renderers.JSONRenderer',
     ]
 }
